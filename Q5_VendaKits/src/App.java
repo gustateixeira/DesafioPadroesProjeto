aee6243e82ac7c2bbe3dbda8bdbe48e3951708e5
BernardoFio <bernardo.fioreze@edu.pucrs.br>
2024-09-08 21:41:23 -0300

    Update App.java

--- Q5_VendaKits/src/App.java
+++ Q5_VendaKits/src/App.java
@@ -5,8 +5,11 @@ public class App {
         VendasFachada proc = new VendasEmMemoria();
         List<Produto> produtos = proc.buscarProdutos();
         Venda venda = proc.iniciarVenda();
-        proc.registrarVenda(venda, produtos.get(0).getId(), 1);
-        proc.registrarVenda(venda, produtos.get(1).getId(), 2);
+        
+        proc.registrarVenda(venda, produtos.get(0).getId(), 1); 
+
+        proc.registrarVenda(venda, produtos.get(2).getId(), 1); 
+
         System.out.println(proc.emitirComprovante(venda));
     }
 }
