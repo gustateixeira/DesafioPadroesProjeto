6950e397da1a329b3ba8a99545701bb19b2513e0
BernardoFio <bernardo.fioreze@edu.pucrs.br>
2024-09-08 21:36:16 -0300

    Update FonteDeDados.java

--- Q4_Visualizadores/src/FonteDeDados.java
+++ Q4_Visualizadores/src/FonteDeDados.java
@@ -4,14 +4,35 @@ import java.util.List;
 
 public class FonteDeDados{
     private List<Integer> lst;
+    private List<Observador> observadores;
+    private List<Observador> listaObservadoresAtivos;
 
     public FonteDeDados(){
         lst = new LinkedList<>();
+        observadores = new ArrayList<>();
+        listaObservadoresAtivos = new ArrayList<>();
+    }
+    
+    public void registraObservador(Observador observador){
+        this.observadores.add(observador);
+        this.notificaObservador();
+    }
+
+    public void notificaObservador() {
+        this.observadores.forEach(obs ->obs.notifica(this.getValores()));
+    }
+
+    public void ativaObservadores(Observador observador){
+        if (!listaObservadoresAtivos.contains(observador)) {
+            listaObservadoresAtivos.add(observador);
+        }
+        observador.ativa();
     }
 
     public void add(Integer value){
         if (value < 0) throw new IllegalArgumentException("Valor invalido");
         lst.add(value);
+        this.notificaObservador();
     }
 
     public int quantidade(){
